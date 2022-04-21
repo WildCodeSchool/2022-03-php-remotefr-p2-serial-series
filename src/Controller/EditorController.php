a8db61543d89ad98dbf8ee1ea0d6fea184fba715
H Rasamy <rasamherve@gmail.com>
2022-04-21 22:47:04 +0200

    us_6 page serial editor brut

---
+++ src/Controller/EditorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+class EditorController extends AbstractController
+{
+    public function index(): string
+    {
+        return $this->twig->render('Editor/index.html.twig');
+    }
+}
+
+
+
+
+
